668cf7d15069635d9fac1a6087133875891008ec
SebasMore <sebasmore1406@gmail.com>
2025-04-08 13:27:48 -0600

    Version 2 now creates all <tr> elements with the function mostrarAlumnos instead of registrarAlumnos.

--- js/main.js
+++ js/main.js
@@ -1,97 +1,69 @@
 const boton = {
-    registrarAlumnos : document.querySelector('#registrar-alumnos'),
-    mostrarAlumnos : document.querySelector('#mostrar-alumnos')
+    registrarAlumnos: document.querySelector('#registrar-alumnos'),
+    mostrarAlumnos: document.querySelector('#mostrar-alumnos')
 };
 
 const tablaAlumnos = document.querySelector('#alumnos');
-const noAlumnos = document.querySelector('#no-alumnos')
+const noAlumnos = document.querySelector('#no-alumnos');
 
 let alumnos = [];
 
-let porRegistrar = ['','','']
-
-boton.registrarAlumnos.addEventListener('click', e => {
+function registrarAlumnos(e) {
+    let porRegistrar = ['', '', ''];
     porRegistrar[0] = prompt('Ingresa el nombre del alumno.');
     porRegistrar[1] = prompt('Ingresa el grupo del alumno.');
     porRegistrar[2] = prompt('Ingresa la matrícula del alumno.');
-    if (porRegistrar[0] === '' || porRegistrar[1] === '' || porRegistrar[2] === '') {
-        alert('Registro no concluido, no se llenaron todos los datos.')
+    if (porRegistrar[0].trim() === '' || porRegistrar[1].trim() === '' || porRegistrar[2].trim() === '') {
+        alert('Registro no concluido, no se llenaron todos los datos.');
     } else {
-        if (alumnos.length === 0) {
-            noAlumnos.style.display = 'none'
-        };
-        alumnos += porRegistrar;
-        let template = `
-        <tr>
-            <td>${porRegistrar[0]}</td>
-            <td>${porRegistrar[1]}</td>
-            <td>${porRegistrar[2]}</td>
-        </tr>
-        `;
-        tablaAlumnos.innerHTML += template;
+        alumnos.push(porRegistrar);
         alert('Datos registrados con éxito.');
     };
     e.preventDefault();
-})
+};
 
-window.addEventListener('keydown', e => {
-    if (e.ctrlKey && e.key === 'r') {
-        porRegistrar[0] = prompt('Ingresa el nombre del alumno.');
-        porRegistrar[1] = prompt('Ingresa el grupo del alumno.');
-        porRegistrar[2] = prompt('Ingresa la matrícula del alumno.');
-        if (porRegistrar[0] === '' || porRegistrar[1] === '' || porRegistrar[2] === '') {
-            alert('Registro no concluido, no se llenaron todos los datos.')
-        } else {
-            if (alumnos.length === 0) {
-                noAlumnos.style.display = 'none'
-            };
-            alumnos += porRegistrar;
-            let template = `
-            <tr>
-                <td>${porRegistrar[0]}</td>
-                <td>${porRegistrar[1]}</td>
-                <td>${porRegistrar[2]}</td>
-            </tr>
-            `;
-            tablaAlumnos.innerHTML += template;
-            alert('Datos registrados con éxito.');
-        };
-    };
-    e.preventDefault();
-})
-
-boton.mostrarAlumnos.addEventListener('click', e => {
-    if (alumnos.length === 0) {
-        if (noAlumnos.style.display === 'block') {
-            noAlumnos.style.display = 'none'
-        } else {
-            noAlumnos.style.display = 'block'
-        };
-    } else {      
-        if (tablaAlumnos.style.display === 'block') {
-            tablaAlumnos.style.display = 'none'
-        } else {
-            tablaAlumnos.style.display = 'block'
-        };
-    }
-    e.preventDefault();
-})
-
-window.addEventListener('keydown', e => {
-    if (e.ctrlKey && e.key === 'm') {     
-        if (Object.keys(alumnos).length === 0) {
-            if (noAlumnos.style.display === 'block') {
-                noAlumnos.style.display = 'none'
-            } else {
-                noAlumnos.style.display = 'block'
-            };
-        } else {      
+function mostrarAlumnos(e) {
+    if (noAlumnos.style.display === 'block') {
+        noAlumnos.style.display = 'none'
+    } else {
+        if (alumnos.length !== 0) {
             if (tablaAlumnos.style.display === 'block') {
                 tablaAlumnos.style.display = 'none'
             } else {
+                let header;
+                header = `
+                <tr>
+                    <th>Nombre</th>
+                    <th>Grupo</th>
+                    <th>Matrícula</th>
+                </tr>
+                `;
+                tablaAlumnos.innerHTML = header;
+                for (let index = 0; index < alumnos.length; index++) {
+                    let template;
+                    template = `
+                    <tr>
+                        <td>${alumnos[index][0]}</td>
+                        <td>${alumnos[index][1]}</td>
+                        <td>${alumnos[index][2]}</td>
+                    </tr>
+                    `;
+                    tablaAlumnos.innerHTML += template;
+                };
                 tablaAlumnos.style.display = 'block'
             };
+        } else {
+            noAlumnos.style.display = 'block'
         };
     };
     e.preventDefault();
-})
+};
+
+boton.registrarAlumnos.addEventListener('click', registrarAlumnos);
+
+boton.mostrarAlumnos.addEventListener('click', mostrarAlumnos);
+
+window.addEventListener('keydown', e => {
+    if (e.ctrlKey && e.key === 'r') { registrarAlumnos(e) };
+    if (e.ctrlKey && e.key === 'm') { mostrarAlumnos(e) };
+});
